26de35b99216343f01126381189847f67cfcaf1f
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-22 21:29:26 -0300

    Card consumindo API

--- Front-End-M5/src/components/CardComp.jsx
+++ Front-End-M5/src/components/CardComp.jsx
@@ -1,22 +1,36 @@
 import Card from 'react-bootstrap/Card';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 function CardComp() {
-  return (
-    <Card>
-      <Card.Header>Quote</Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p>
-            {' '}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante.{' '}
-          </p>
-          <footer className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+    const [titulo,setTitulo]=useState('')
+    const [descricao,setDescricao]=useState('')
+    const [data_criacao,setData_criacao]=useState('')
+    useEffect(() => {
+        const options = { method: 'GET', url: 'http://localhost:3000/tarefa' };
+        axios.request(options).then(function (response) {
+            setTitulo(response.data[0].titulo)
+            setDescricao(response.data[0].descricao)
+            setData_criacao(response.data[0].data_criacao)
+        }).catch(function (error) {
+            console.error(error);
+        });
+    },[])
+
+    return (
+        <Card>
+            <Card.Header>{titulo}</Card.Header>
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <p>
+                        {descricao}
+                    </p>
+                    <footer className="blockquote-footer">
+                        {data_criacao}
+                    </footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default CardComp;

a240edc67784fbb5b71e958fee4ffba4d7e14dd9
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-28 15:25:07 -0300

    28-03

--- Front-End-M5/src/components/CadastroFormModal copy.jsx
+++ Front-End-M5/src/components/CadastroFormModal copy.jsx
@@ -4,12 +4,79 @@ import Modal from 'react-bootstrap/Modal';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import axios from 'axios';
 
 function CadastroFormModal() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [CEP, setCEP] = useState('')
+  const [endereco, setEndereco] = useState('')
+  const [complemento, setComplemento] = useState('')
+  const [CPF, setCPF] = useState('')
+  const [numero, setNumero] = useState('')
+  const [image, setImage] = useState('')
+  const [rSenha, setRSenha] = useState('')
+  const [cidade, setCidade] = useState('')
+  const [estado, setEstado] = useState('')
+  const [fotoPerfil,setFotoPerfil]=useState('')
+
+  function Cadastro() {
+    if (senha != rSenha) {
+      setMsn('As senhas não conferem.')
+    } else {
+      const cadUser = {
+        nome: nome,
+        email: email,
+        senha: senha,
+        CEP: CEP,
+        endereco: endereco,
+        complemento: complemento,
+        CPF: CPF,
+        numero: numero,
+        cidade: cidade,
+        estado: estado
+      }
+        (async () => {
+          axios.post('http://localhost:3000/usuario', cadUser)
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
+        })
+    }
+  }
+  function enviaFoto() {
+    const blob = image.slice(0, image.size, 'image/jpg'); 
+    const newImage = new File([blob], `${nome}.jpg`, {type: 'image/jpg'});
+    const form = new FormData();
+    form.append('image', newImage,);
+
+    const options = {
+      method: 'POST',
+      url: 'http://localhost:3000/usuario/upload',
+      headers: {'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001'},
+      data: form
+    }
+
+    axios.request(options)
+    .then((response) => {
+      console.log(response.data)
+      console.log(image)
+    }).catch((err) => {
+      console.log(err)
+    })
+    
+    setFotoPerfil()
+  }
+
 
   return (
     <>
@@ -27,17 +94,29 @@ function CadastroFormModal() {
             <Row className="mb-3">
               <Form.Group className="mb-3" controlId="formGridAddress2">
                 <Form.Label>Nome:</Form.Label>
-                <Form.Control placeholder="Seu nome" />
+                <Form.Control placeholder="Seu nome" onChange={(e) => setNome(e.target.value)} />
               </Form.Group>
-
+            </Row>
+            <Row className="mb-3">
+            <Form.Label>Foto do perfil:</Form.Label>
+            
+              <Form.Group as={Col}>
+                <Form.Control type='file' onChange={(e) => { setImage(e.target.files[0]) }} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Button variant="secondary" onClick={enviaFoto}>enviar</Button>
+              </Form.Group>
+              <img src={fotoPerfil} width='50'/>
+            </Row>
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridEmail">
                 <Form.Label>Email:</Form.Label>
-                <Form.Control type="email" placeholder="Seu email" />
+                <Form.Control type="email" placeholder="Seu email" onChange={(e) => setEmail(e.target.value)} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridZip">
                 <Form.Label>CPF:</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={(e) => setCPF(e.target.value)} />
               </Form.Group>
 
             </Row>
@@ -45,58 +124,58 @@ function CadastroFormModal() {
 
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Senha:</Form.Label>
-                <Form.Control type="password" placeholder="senha" />
+                <Form.Control type="password" placeholder="senha" onChange={(e) => setSenha(e.target.value)} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Repita senha:</Form.Label>
-                <Form.Control type="password" placeholder="repita senha" />
+                <Form.Control type="password" placeholder="repita senha" onChange={(e) => setRSenha(e.target.value)} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridZip">
                 <Form.Label>CEP:</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={(e) => setCEP(e.target.value)} />
               </Form.Group>
 
             </Row>
 
             <Row className="mb-3">
-            <Col xs={9}>
+              <Col xs={9}>
 
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Endereço:</Form.Label>
-              <Form.Control placeholder="Rua A" />
-            </Form.Group>
-            </Col>
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                  <Form.Label>Endereço:</Form.Label>
+                  <Form.Control placeholder="Rua A" onChange={(e) => setEndereco(e.target.value)} />
+                </Form.Group>
+              </Col>
 
-            <Col xs={3}>
-            <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>Número:</Form.Label>
-                <Form.Control />
-              </Form.Group>
+              <Col xs={3}>
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>Número:</Form.Label>
+                  <Form.Control onChange={(e) => setNumero(e.target.value)} />
+                </Form.Group>
 
-            </Col>
+              </Col>
             </Row>
 
             <Row className="mb-3">
 
               <Form.Group as={Col} controlId="formGridCity">
                 <Form.Label>Complemento:</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={(e) => setComplemento(e.target.value)} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridCity">
                 <Form.Label>Cidade:</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={(e) => setCidade(e.target.value)} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridState">
                 <Form.Label>Estado:</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={(e) => setEstado(e.target.value)} />
               </Form.Group>
             </Row>
             <Form.Group className="mb-3" id="formGridCheckbox">
-              <Form.Check type="checkbox" label="Aceito os termos e condições." />
+              <Form.Check type="checkbox" label="Aceito os termos e condições." onChange={(e) => setAceito(e.target.value)} />
             </Form.Group>
           </Form>
 
@@ -105,7 +184,7 @@ function CadastroFormModal() {
           <Button variant="secondary" onClick={handleClose}>
             Fechar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={Cadastro}>
             Cadastrar-se
           </Button>
         </Modal.Footer>
--- Front-End-M5/src/components/LoginFormModal.jsx
+++ Front-End-M5/src/components/LoginFormModal.jsx
@@ -1,18 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios'
 
 function LoginFormModal() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [resp, setResp] = useState('')
+  const [resposta, setResposta] = useState('')
+  const [logado, setLogado] = useState(false)
+  const [botao, setBotao] = useState('login')
+  function logar() {
+    const user = {
+      senha: senha
+    }
+    if (email === '' && senha === '') {
+      setResposta('Insira um email e uma senha.')
+    } else {
+      axios.post(`http://localhost:3000/usuario/login/${email}`, user)
+        .then(function (response) {
+          console.log(response.data);
+          setResp(response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  useEffect(() => {
+    if (resp != true) {
+      if (resp === false) {
+        setResposta('senha incorreta')
+      } else {
+        setResposta(resp)
+      }
+    } else {
+      setResposta('')
+      setShow(false)
+      setLogado(true)
+      setBotao('logout')
+    }
+  }, [resp])
   return (
     <>
-      <Button variant="primary" size="sm" onClick={handleShow}>
-        login
+      <Button variant="primary" size="sm" onClick={() => {
+        if (!logado) {
+          handleShow()
+        } else {
+          setLogado(false)
+          setBotao('login')
+        }
+      }}>
+        {botao}
       </Button>
 
       <Modal show={show} onHide={handleClose}>
@@ -20,30 +64,28 @@ function LoginFormModal() {
           <Modal.Title>Login de Usuários</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control type="email" placeholder="email" />
-        <Form.Text className="text-muted">
-          Entre com seu email cadastrado.
-        </Form.Text>
-      </Form.Group>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">
+                Entre com seu email cadastrado.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>senha:</Form.Label>
-        <Form.Control type="password" placeholder="senha" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>senha:</Form.Label>
+              <Form.Control type="password" placeholder="senha" onChange={(e) => setSenha(e.target.value)} />
+            </Form.Group>
+            <p>{resposta}</p>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Fechar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" onClick={logar}>
+            login
           </Button>
         </Modal.Footer>
       </Modal>
